de8c810fa39b0184555305657c58540dfa5dc6a6
Jesús José <jaredmaster32@gmail.com>
2019-02-09 23:31:00 -0700

    termina exercise2.js

--- exercise2.js
+++ exercise2.js
@@ -23,9 +23,69 @@ Output: Yes
 function ex2(arr){
     let div3 = [];
     let div5 = [];
-    let other = [];
-    arr.forEach(l => {
+    let other = ['x'];
+    arr.forEach(l => {s
         l % 5 == 0 ? div5.push(l) : l % 3 == 0 ? div3.push(l) : other.push(l);
     });
-    return [div3, div5, other];
+    other = heapsMethod(other);
+    for(p in other){
+        if(
+            div3.concat(p.slice(0,p.indexOf('x'))).reduce((a,b)=>a+b) ===
+            div5.concat(p.slice(p.indexOf('x')+1)).reduce((a,b)=>a+b)
+        ){
+            return true;
+        }
+    }
+    return false;
+}
+
+/*
+> console.log(arr.map(l=> [   [ l[0], l[1] ], [ l[2], l[3] ]   ]   ));
+[ [ [ 1, 2 ], [ 3, 4 ] ],
+  [ [ 5, 6 ], [ 7, 8 ] ],
+  [ [ 9, 10 ], [ 11, 12 ] ] ]
+
+*/
+
+/*
+> [1,2,3,4].slice(0,[1,2,3,4].indexOf(3))
+[ 1, 2 ]
+> [1,2,3,4].slice([1,2,3,4].indexOf(3))
+[ 3, 4 ]
+
+*/
+
+
+//Este algoritmo lo adapté del que se muestra en wikipedia https://en.wikipedia.org/wiki/Heap%27s_algorithm
+function heapsMethod(arr){
+    const length = arr.length;
+    let c = [];
+    let result = [];
+    for(let i = 0; i < length; i++) {
+        c[i] = 0;
+    }
+
+    //Es necesario hacer una copia profunda
+    result.push(JSON.parse(JSON.stringify(arr)));
+    let i = 0;
+    while(i < length){
+        if (c[i] < i) {
+            if(i % 2 === 0) {
+                let t = arr[0];
+                arr[0] = arr[i];
+                arr[i] = t;
+            } else {
+                let t = arr[c[i]];
+                arr[c[i]] = arr[i];
+                arr[i] = t;
+            }
+            result.push(JSON.parse(JSON.stringify(arr)));
+            c[i]++;
+            i = 0;
+        }else{
+            c[i] = 0;
+            i++;
+        }
+    } 
+    return result;
 }
